4197e1fd5c28536e2caa71aa3e0d5cf9ab344d6f
Mesut Kaçmaz <mesut.kacmaz@outlook.com>
2021-06-17 20:02:38 +0300

    Added bootstrap and font awesome & created Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className='navbar navbar-dark bg-dark mb-5'>
+      <span className='navbar-brand mb-0 h1 mx-auto'>LyricFinder</span>
+    </nav>
+  )
+}
+
+export default Navbar
