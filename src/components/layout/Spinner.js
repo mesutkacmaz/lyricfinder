6fcab4489ed203a1f51fc7dca695b1128bc22a86
Mesut Kaçmaz <mesut.kacmaz@outlook.com>
2021-06-20 16:00:21 +0300

    Created a spinner component for user experience

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,11 @@
+import spinner from './spinner.gif'
+
+const Spinner = () => {
+  return (
+    <div>
+      <img src={spinner} style={{ width: '200px', margin: '40px auto', display: 'block' }} alt='spinner' />
+    </div>
+  )
+}
+
+export default Spinner
