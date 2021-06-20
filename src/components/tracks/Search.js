5eff83b8b30ea215b1cf1e3ca5cf805a3846c2f1
Mesut Kaçmaz <mesut.kacmaz@outlook.com>
2021-06-20 15:57:32 +0300

    Added search functionality

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -1,7 +1,27 @@
-import { useState } from 'react'
+import { useState, useContext} from 'react'
+import axios from 'axios'
+import { GlobalContext } from '../../context/GlobalState'
 
 const Search = () => {
   const [trackTitle, setTrackTitle] = useState('')
+
+  const { dispatch } = useContext(GlobalContext)
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const res = await axios.get(`https://cors-access-allow.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`)
+      dispatch({
+        type: 'SEARCH_TRACKS',
+        payload: res.data.message.body.track_list
+      })
+
+      setTrackTitle('')
+    } catch (err) {
+      console.error(err)
+    }
+  }
 
   return (
       <div className='card card-body mb-4 p-4'>
@@ -9,7 +29,7 @@ const Search = () => {
           <i className='fas fa-music'></i> Search For A Song
         </h1>
         <p className='lead text-center'>Get The Lyrics For Any Song</p>
-        <form>
+        <form onSubmit={onSubmit}>
           <div className='form-group'>
             <input type='text' className='form-control form-control-lg mb-1' placeholder='Song Title...' value={trackTitle} onChange={e => setTrackTitle(e.target.value)} />
           </div>
