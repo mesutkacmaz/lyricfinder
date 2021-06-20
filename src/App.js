7435ce19efa646d8738998fcc44986ed0ae64b0e
Mesut Kaçmaz <mesut.kacmaz@outlook.com>
2021-06-20 16:02:49 +0300

    Added router and context provider in main App component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Index from './components/layout/Index'
+import { GlobalProvider } from './context/GlobalState'
+import Lyrics from './components/tracks/Lyrics'
 
 const App = () => {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <div className='container'>
-          <Switch>
-            <Route path='/' component={Index} exact />
-          </Switch>
-        </div>
-      </>
-    </Router>
+    <GlobalProvider>
+      <Router>
+        <>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route path='/' component={Index} exact />
+              <Route path='/lyrics/track/:id' component={Lyrics} exact />
+            </Switch>
+          </div>
+        </>
+      </Router>
+    </GlobalProvider>
   );
 }
 
